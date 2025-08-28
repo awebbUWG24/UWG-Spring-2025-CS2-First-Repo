bb99f33d582eefd45c936f020103531b618d3046
Aiden Webb <awebb22@my.westga.edu>
2025-08-27 22:32:48 -0400

    Poor Seperation Of Concerns
    1) Created a new view package
    2) Added the BillView class
    3) Moved and refactored the getText()
    method to work inside the BillView class

---
+++ code/starterLab1/Lab 1/src/edu/westga/cs1302/lab1/view/BillView.java
@@ -0,0 +1,41 @@
+package edu.westga.cs1302.lab1.view;
+
+import edu.westga.cs1302.lab1.model.BillItem;
+import edu.westga.cs1302.lab1.model.Bill;
+
+/** Displays the list of items in a bill
+ * 
+ * @author Aiden_Webb CS1302
+ * @version Fall 2025
+ */
+public class BillView {
+	
+	private Bill itemList = new Bill();
+	
+	/** Return a String containing the list of bill items and total for the bill.
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return a String containing the list of bill items and total for the bill
+	 */
+	public String getText() {
+		String text = "ITEMS" + System.lineSeparator();
+		double subTotal = 0.0;
+		for (BillItem item : this.itemList.getItems()) {
+			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
+			subTotal += item.getAmount();
+		}
+		
+		text += System.lineSeparator();
+		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
+		double tax = subTotal * 0.1;
+		double tip = subTotal * 0.2;
+		text += "TAX - $" + tax + System.lineSeparator();
+		text += "TIP - $" + tip + System.lineSeparator();
+		text += "TOTAL - $" + (subTotal + tip + tax);
+		
+		return text;
+	}
+
+}
