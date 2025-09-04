a9021f47b5ac459862011c3789af23ae4c7ef2e2
Aiden Webb <awebb22@my.westga.edu>
2025-09-04 00:51:05 -0400

    Created First Test Class
    1) Created the TestConstructor class
    2) Created the TestProperInput method

---
+++ code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill_item/TestConstructor.java
@@ -0,0 +1,26 @@
+package edu.westga.cs1302.lab2.tests.model.bill_item;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import edu.westga.cs1302.lab2.model.BillItem;
+
+import org.junit.jupiter.api.Test;
+
+class TestConstructor {
+	
+	@Test
+	public void TestProperInput() {
+		String name = "Chicken";
+		double price = 5.0;
+		
+		BillItem testItem = new BillItem(name, price);
+		String resultName = testItem.getName();
+		double resultAmount = testItem.getAmount();
+		
+		assertEquals("Chicken", resultName);
+		assertEquals(5.0, resultAmount, 0.001);
+	}
+
+}
