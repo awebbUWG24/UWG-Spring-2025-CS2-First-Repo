96dd8da0b79f56ef09dbaa6c15caea774a10a94d
Aiden Webb <awebb22@my.westga.edu>
2025-09-04 04:28:20 -0400

    Added New Test Class and Test Method
    1) Created the TestAddItem class
    2) Made the TestAddItemWhenItemIsValid method

---
+++ code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill/TestAddItem.java
@@ -0,0 +1,28 @@
+package edu.westga.cs1302.lab2.tests.model.bill;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+
+class TestAddItem {
+
+	@Test
+	public void TestAddItemWhenItemIsValid() {
+		BillItem testItem = new BillItem("Chicken", 5.0);
+		BillItem testItem2 = new BillItem("Apple", 2.0);
+		Bill testBill = new Bill();
+		
+		testBill.addItem(testItem);
+		testBill.addItem(testItem2);
+		String resultName = testBill.getItems().get(0).getName();
+		double resultAmount = testBill.getItems().get(0).getAmount();
+		
+		assertEquals("Chicken", resultName);
+		assertEquals(5.0, resultAmount, 0.001);
+	}
+
+}
