af87bfb72b90e784d28f56fba98b3ca523ef1592
Aiden Webb <awebb22@my.westga.edu>
2025-09-10 21:26:21 -0400

    Creating a Bill Display
    1) Created the displaythemod to recieve
    user input and display it on a Bill
    2) Created a catch in the scenario of
    an improper amount input

--- code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,7 +1,12 @@
 package edu.westga.cs1302.lab3.views;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -10,18 +15,42 @@ import javafx.scene.control.TextField;
  * @version Fall 2025
  */
 public class MainWindow {
+	private Bill billItems = new Bill();
 	
     @FXML
     private TextField amount;
 
     @FXML
     private TextField name;
-
+    
     @FXML
-    void additem(ActionEvent event) {
-
+    private TextArea billDisplay;
+    
+    /** Add a created item to the bill and update the bill to match
+     *
+     * @param event on button press
+     * 
+     */
+    @FXML
+    void addItem(ActionEvent event) {
+    	
+    	try {
+    		String newItemName = this.name.getText();
+        	double newItemPrice = Double.parseDouble(this.amount.getText());
+        	
+        	BillItem newItem = new BillItem(newItemName, newItemPrice);
+        	
+        	this.billItems.addItem(newItem);
+        	
+        	billDisplay.setText(BillView.getText(billItems));
+    	}
+    	catch(NumberFormatException e) {
+    		Alert alert = new Alert(Alert.AlertType.ERROR);
+    		alert.setContentText("Invalid amount. Please input a positive non zero number.");
+    		alert.showAndWait();
+    	}
+    	
     }
-
     
     /**
      * Perform any needed initialization of UI components and underlying objects.
