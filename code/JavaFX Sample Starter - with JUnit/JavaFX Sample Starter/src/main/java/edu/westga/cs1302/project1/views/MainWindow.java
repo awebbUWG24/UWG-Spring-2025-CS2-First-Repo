bb349dd4b92ad723ceae114811df0223da83f2a4
Aiden Webb <awebb22@my.westga.edu>
2025-09-29 02:53:56 -0400

    A – UI for Adding Tasks
    Added JavaFx components for adding task
    components and viewing list of tasks

---
+++ code/JavaFX Sample Starter - with JUnit/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/project1/views/MainWindow.java
@@ -0,0 +1,35 @@
+package edu.westga.cs1302.project1.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * Controller class for drawing various things to our canvas window.
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class MainWindow {
+	
+	@FXML
+    private TextArea taskDesc;
+
+    @FXML
+    private ListView<String> taskList;
+
+    @FXML
+    private TextField taskName;
+
+    @FXML
+    private ComboBox<String> taskPrio;
+    
+    /**
+     * Perform any needed initialization of UI components and underlying objects.
+     */
+    public void initialize() {
+    	
+    }
+}
