e97938c105036a1cbc0cf7825a02d9b307b8ee13
Aiden Webb <awebb22@my.westga.edu>
2025-12-08 01:29:30 -0500

    1B – Model for Storing Collections
    Added a class to support storing a collection

---
+++ code/project3Starter/Task Tracker/src/main/java/edu/westga/cs1302/comic_collections/model/ComicCollection.java
@@ -0,0 +1,18 @@
+package edu.westga.cs1302.comic_collections.model;
+
+/** Stores Comics in a collection
+ * 
+ * @author AidenWebb
+ * @version Fall 2025
+ */
+public class ComicCollection {
+	
+	private String collectionName;
+	
+	/**
+	 * Creates a comic collection
+	 */
+	public ComicCollection() {
+		this.collectionName = "";
+	}
+}
