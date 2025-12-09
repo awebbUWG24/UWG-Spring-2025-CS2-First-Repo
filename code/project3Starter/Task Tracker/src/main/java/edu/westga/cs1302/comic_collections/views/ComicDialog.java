1df0531c7732a01d49a1d703d9a4790fb6c08599
Aiden Webb <awebb22@my.westga.edu>
2025-12-09 08:25:50 -0500

    2D – Binding View and ViewModel for Adding Comic
    Attempted to bind UI elements to
    view model to create a display.
    
    Did not succeed.

--- code/project3Starter/Task Tracker/src/main/java/edu/westga/cs1302/comic_collections/views/ComicDialog.java
+++ code/project3Starter/Task Tracker/src/main/java/edu/westga/cs1302/comic_collections/views/ComicDialog.java
@@ -1,11 +1,14 @@
 package edu.westga.cs1302.comic_collections.views;
 
 import edu.westga.cs1302.comic_collections.model.Comic;
+import edu.westga.cs1302.comic_collections.viewmodel.Project3ViewModel;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 /**
  * Display for creating a new comic
@@ -20,6 +23,7 @@ public class ComicDialog extends Dialog<Comic> {
 	 @FXML private TextField issueNumberField;
 	 
 	 private Stage dialogStage;
+	 private Project3ViewModel vm;
 	 
 	 @FXML
 	 private void initialize() {
@@ -34,4 +38,23 @@ public class ComicDialog extends Dialog<Comic> {
 	 public void setDialogStage(Stage dialogStage) {
 		 this.dialogStage = dialogStage;
 	 }
+	 
+	private void bindUIElements() {
+		this.comicNameField.textProperty().bindBidirectional(this.vm.getComicTitle());
+		this.issueNumberField.textProperty().bindBidirectional(this.vm.getComicIssueNumber(), new NumberStringConverter());
+	}
+	
+	private void bindBehavior() {
+		this.addComic.setOnAction(
+				(event) -> {
+					this.vm.addNewComic();
+				}
+		);
+		
+		this.cancel.setOnAction(
+				(event) -> {
+					((Node) (this.addComic)).getScene().getWindow().hide();
+				}
+		);
+	}
 }
